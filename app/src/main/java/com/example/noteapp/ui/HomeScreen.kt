6421c456ab0adc17ac2bf904e0310b176e58dcd2
Bi122e <luonggiakietk1@gmail.com>
2026-08-18 16:28:38 +0700

    update home ui v2

--- app/src/main/java/com/example/noteapp/ui/HomeScreen.kt
+++ app/src/main/java/com/example/noteapp/ui/HomeScreen.kt
@@ -1,29 +1,44 @@
 package com.example.noteapp.ui
 
+import android.R
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldColors
-import androidx.compose.runtime.Composable
+ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+ import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -39,13 +54,19 @@ fun HomeScreen() {
     Scaffold(
         modifier = Modifier,
         topBar = { TopBar() },
+        bottomBar = { BottomNoteBar() },
         containerColor = Color.Black
     ) { paddingValues ->
 
 
         Column(
             modifier = Modifier
-                .padding(paddingValues),
+                .padding(
+                    paddingValues = PaddingValues(
+                        vertical = paddingValues.calculateTopPadding(),
+                        horizontal = 16.dp
+                        )
+                ),
 
         ) {
 
@@ -55,19 +76,28 @@ fun HomeScreen() {
                 active = active,
                 onActiveChange = {  }
             )
-        }
-        if (items.isNotEmpty()) {
 
-            LazyColumn(contentPadding = paddingValues, ) {
+            if (items.isNotEmpty()) {
 
-                items(items) { item ->
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(2),
+                    modifier = Modifier.fillMaxSize(),
+                    contentPadding = paddingValues,
+                    verticalArrangement = Arrangement.spacedBy(0.dp),
+                    horizontalArrangement = Arrangement.spacedBy(0.dp)
+                ) {
 
+                    items(items) { noteItem ->
 
+                        NoteItem()
+
+                    }
                 }
-            }
-        } else {
+            } else {
 
+            }
         }
+
     }
 }
 
@@ -77,8 +107,47 @@ fun NoteItem() {
     Box(
         modifier = Modifier
             .width(250.dp)
+            .heightIn(min = 150.dp, max = 200.dp)
             .padding(10.dp)
+            .background(
+                Color(0xFF616161),
+                RoundedCornerShape(12.dp)
+                )
+            .padding(
+                vertical = 24.dp,
+                horizontal = 10.dp
+            )
     ) {
+
+       Column(
+           verticalArrangement = Arrangement.spacedBy(6.dp),
+
+       ) {
+
+           Text(
+               text = "testfdfdfdffdfdfdffdfdf",
+               color = Color.White,
+               fontSize = 16.sp,
+               maxLines = 1,
+               overflow = TextOverflow.Ellipsis
+           )
+
+            Text(
+               text = "testfdfdfdffdfdfdffdfdf",
+               color = Color(0xFFCFCFCF),
+               fontSize = 16.sp,
+               maxLines = 1,
+               overflow = TextOverflow.Ellipsis
+           )
+
+           Spacer(Modifier.height(16.dp))
+
+           Text(
+               text = "1:05 PM",
+               color = Color(0xFFDBDBDB),
+               fontSize = 12.sp
+           )
+       }
 
     }
 
@@ -169,3 +238,41 @@ data class Note(
     val content: String = "",
 
     )
+
+
+@Composable
+fun BottomNoteBar() {
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                vertical = 32.dp,
+                horizontal = 16.dp
+            ),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.End
+    ) {
+
+
+        Box(
+            modifier = Modifier
+                .size(60.dp)
+                .background(
+                    color = Color(0xFFFFBF00),
+                    CircleShape
+                )
+                .padding(10.dp),
+            contentAlignment = Alignment.Center
+        ) {
+
+            Icon(
+                modifier = Modifier.fillMaxSize(),
+                imageVector = Icons.Rounded.Add,
+                tint = Color.White,
+                contentDescription = null
+            )
+        }
+
+    }
+}
